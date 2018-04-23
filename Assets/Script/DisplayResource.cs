75b83fb7869987917d45e3d0b4d7c68e64e882ae
SonKatarina23 <houwenlie98@gmail.com>
2018-04-24 06:10:16 +0700

    April 24th Few Minor things

---
+++ Assets/Script/DisplayResource.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayResource : MonoBehaviour {
+
+	private int tempCooper, tempGold, tempIron, tempRock, tempSilver, tempWood;
+	public Text cooperT, goldT, ironT, rockT, silverT, woodT;
+
+	void Update() {
+		tempCooper = PlayerPrefs.GetInt ("Cooper", 0);
+		tempGold = PlayerPrefs.GetInt ("Gold", 0);
+		tempIron = PlayerPrefs.GetInt ("Iron", 0);
+		tempRock = PlayerPrefs.GetInt ("Rock", 0);
+		tempSilver = PlayerPrefs.GetInt ("Silver", 0);
+		tempWood = PlayerPrefs.GetInt ("Wood", 0);
+
+		cooperT.text = tempCooper.ToString();
+		goldT.text = tempGold.ToString();
+		ironT.text = tempIron.ToString();
+		rockT.text = tempRock.ToString();
+		silverT.text = tempSilver.ToString();
+		woodT.text = tempWood.ToString();
+	}
+}
