6cb8f92b56a9f5110bb4346252bb4484f3104e5c
SonKatarina23 <houwenlie98@gmail.com>
2018-04-26 13:19:43 +0700

    April 26th
    Crafting cost and loading

---
+++ Assets/Script/CraftingConfirmation.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CraftingConfirmation : MonoBehaviour {
+
+	private int tempCooper, tempGold, tempIron, tempRock, tempSilver, tempWood;
+
+	public Slider loading;
+	private int MaterialID;
+	// 1 = Copper, 2 = Gold, 3 = Iron, 4 = Silver, 5 = Rock
+	public Text NotEnoughMaterial;
+
+	private bool AllowCraft;
+
+	public float loadingTime;
+	public float delayBeforeDisappear;
+	private float counter = 0f;
+
+	void Awake() {
+		MaterialID = PlayerPrefs.GetInt ("MaterialID");
+	}
+
+	void Start() {
+		loading.value = 0f;
+		loading.gameObject.SetActive (false);
+
+		NotEnoughMaterial.gameObject.SetActive (false);
+		AllowCraft = false;
+	}
+
+	public void Accept() {
+		switch (MaterialID) {
+		case 1:
+			if (tempCooper >= 1 && tempWood >= 1) {
+				AllowCraft = true;
+				tempCooper--;
+				PlayerPrefs.SetInt ("Cooper", tempCooper);
+			}
+			break;
+
+		case 2:
+			if (tempGold >= 1 && tempWood >= 1) {
+				AllowCraft = true;
+				tempGold--;
+				PlayerPrefs.SetInt ("Gold", tempCooper);
+			}
+			break;
+
+		case 3:
+			if (tempIron >= 1 && tempWood >= 1) {
+				AllowCraft = true;
+				tempIron--;
+				PlayerPrefs.SetInt ("Iron", tempCooper);
+			}
+			break;
+
+		case 4:
+			if (tempSilver >= 1 && tempWood >= 1) {
+				AllowCraft = true;
+				tempSilver--;
+				PlayerPrefs.SetInt ("Silver", tempCooper);
+			}
+			break;
+		
+		case 5:
+			if (tempRock >= 1 && tempWood >= 1) {
+				AllowCraft = true;
+				tempRock--;
+				PlayerPrefs.SetInt ("Rock", tempCooper);
+			}
+			break;
+		
+		}
+
+		if (AllowCraft == false) {
+			NotEnoughMaterial.gameObject.SetActive (true);
+		} else {
+			NotEnoughMaterial.gameObject.SetActive (false);
+		}
+
+
+	}
+
+	void Update() {
+		tempCooper = PlayerPrefs.GetInt ("Cooper", 0);
+		tempGold = PlayerPrefs.GetInt ("Gold", 0);
+		tempIron = PlayerPrefs.GetInt ("Iron", 0);
+		tempRock = PlayerPrefs.GetInt ("Rock", 0);
+		tempSilver = PlayerPrefs.GetInt ("Silver", 0);
+		tempWood = PlayerPrefs.GetInt ("Wood", 0);
+
+		if (AllowCraft == true) { //MUNCULIN LOADING NYE
+			loading.gameObject.SetActive (true);
+			counter += Time.deltaTime;
+			loading.value = counter / loadingTime;
+
+			if (counter >= loadingTime + delayBeforeDisappear) {
+				loading.gameObject.SetActive (false);
+				AllowCraft = false;
+			}
+		}
+	}
+}
---
+++ Assets/Script/DisplayCost.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayCost : MonoBehaviour {
+
+	private int MaterialID;
+	// 1 = Copper, 2 = Gold, 3 = Iron, 4 = Silver, 5 = Rock
+
+	public GameObject ReCooper, ReGold, ReIron, ReSilver, ReRock;
+	//Re mksd nya Resource (sprite resourcenye)
+
+	public Text CostOtherWood;
+	public Text CostWood;
+
+	void Awake() {
+		MaterialID = PlayerPrefs.GetInt ("MaterialID");
+	}
+
+	void Start() {
+		CostWood.text = "1";
+		CostOtherWood.text = "1";
+
+		switch(MaterialID) {
+		case 1:
+			ReCooper.gameObject.SetActive (true); 
+			break;
+		case 2:
+			ReGold.gameObject.SetActive (true);
+			break;
+		case 3: 
+			ReIron.gameObject.SetActive (true);
+			break;
+		case 4:
+			ReSilver.gameObject.SetActive (true);
+			break;
+		case 5:
+			ReRock.gameObject.SetActive (true);
+			break;
+		}
+	}
+}
