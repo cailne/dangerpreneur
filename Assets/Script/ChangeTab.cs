da70738b86935502c365f05a6da3e250dfa36219
SonKatarina23 <houwenlie98@gmail.com>
2018-05-14 05:45:51 +0700

    Sebelum Presentasi

--- Assets/Script/ChangeTab.cs
+++ Assets/Script/ChangeTab.cs
@@ -3,26 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeTab : MonoBehaviour {
-	public GameObject TabBuy;
+
+	//Selected tabs
+	public GameObject TabBuy; 
 	public GameObject TabHire;
 	public GameObject TabSell;
+
+	//Unselected tabs
+	public GameObject UnselectBuy, UnselectHire, UnselectSell;
+
+	void Start() {
+		ChangeToBuy ();
+	}
 
 	public void ChangeToBuy(){
 		TabHire.SetActive (false);
 		TabSell.SetActive (false);
 		TabBuy.SetActive (true);
+
+		UnselectBuy.SetActive (false);
+		UnselectHire.SetActive (true);
+		UnselectSell.SetActive (true);
 	}
 
 	public void ChangeToHire(){
 		TabBuy.SetActive (false);
 		TabSell.SetActive (false);
 		TabHire.SetActive (true);
+
+		UnselectBuy.SetActive (true);
+		UnselectHire.SetActive (false);
+		UnselectSell.SetActive (true);
 	}
 
 	public void ChangeToSell(){
 		TabBuy.SetActive (false);
 		TabHire.SetActive (false);
 		TabSell.SetActive (true);
+
+		UnselectBuy.SetActive (true);
+		UnselectHire.SetActive (true);
+		UnselectSell.SetActive (false);
 	}
 
 }
