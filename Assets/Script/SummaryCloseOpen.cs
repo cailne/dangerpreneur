fc199215042c5c5c3ec353b0b3726894b2801c58
GagasW <kurnia.walandri@binus.ac.id>
2018-06-23 22:31:20 +0700

    Summary

---
+++ Assets/Script/SummaryCloseOpen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummaryCloseOpen : MonoBehaviour {
+	public GameObject Summary;
+	// Use this for initialization
+	void Start () {
+		Summary.SetActive (false);
+	}
+
+	public void OpenSummary(){
+		Summary.SetActive(true);
+	}
+
+	public void CloseSummary(){
+		Summary.SetActive (false);
+	}
+}
