c476a3225b0a3d856715d261997451c9f976d2f5
SonKatarina23 <houwenlie98@gmail.com>
2018-06-27 11:03:10 +0700

    Add Encyclopedia
    Tolong backgroundnya diganti nanti ye itu background sementara doang

---
+++ Assets/Script/Encyclopedia.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Encyclopedia : MonoBehaviour {
+
+	public GameObject EncyJenawi, EncySiwar, EncyTrisula, EncyGolok;
+
+	/*void Awake() {
+		EncyJenawi = GameObject.Find ("EncyclopediaJenawi");
+		EncySiwar = GameObject.Find ("EncyclopediaSiwar");
+		EncyTrisula = GameObject.Find ("EncyclopediaTrisula");
+		EncyGolok = GameObject.Find ("EncyclopediaGolok");
+	}*/
+
+	void OnMouseEnter() {
+		if (gameObject.name == "Jenawi") {
+			EncyJenawi.SetActive (true);
+			EncySiwar.SetActive (false);
+			EncyTrisula.SetActive (false);
+			EncyGolok.SetActive (false);
+
+		} else if (gameObject.name == "Siwar") {
+			EncyJenawi.SetActive (false);
+			EncySiwar.SetActive (true);
+			EncyTrisula.SetActive (false);
+			EncyGolok.SetActive (false);
+			Debug.Log ("SIWAR ADA");
+
+		} else if (gameObject.name == "Trisula") {
+			EncyJenawi.SetActive (false);
+			EncySiwar.SetActive (false);
+			EncyTrisula.SetActive (true);
+			EncyGolok.SetActive (false);
+
+		} else if (gameObject.name == "Golok") {
+			EncyJenawi.SetActive (false);
+			EncySiwar.SetActive (false);
+			EncyTrisula.SetActive (false);
+			EncyGolok.SetActive (true);
+		}
+	}
+
+
+	void OnMouseExit() {
+		EncyJenawi.SetActive (false);
+		EncySiwar.SetActive (false);
+		EncyTrisula.SetActive (false);
+		EncyGolok.SetActive (false);
+	}
+
+	/*public void ClosePopUp() {
+		gameObject.SetActive (false);
+	}*/
+}
