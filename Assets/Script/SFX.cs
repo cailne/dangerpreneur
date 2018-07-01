0dfd933019d057272e7d68d1f78fad7aadbcb2f3
SonKatarina23 <houwenlie98@gmail.com>
2018-07-01 18:04:54 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/cailne/dangerpreneur""
    This reverts commit e25718a7b33f8c4d3715eb25aaa18f40351ad2d7.

---
+++ Assets/Script/SFX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX : MonoBehaviour {
+
+    public static SFX instance;
+    public AudioSource source;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetVolume(float x)
+    {
+        source.volume = x;
+    }
+
+    public float GetVolume()
+    {
+        return source.volume;
+    }
+}
