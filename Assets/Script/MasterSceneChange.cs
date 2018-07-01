e25718a7b33f8c4d3715eb25aaa18f40351ad2d7
SonKatarina23 <houwenlie98@gmail.com>
2018-07-01 18:02:05 +0700

    Revert "Merge branch 'master' of https://github.com/cailne/dangerpreneur"
    This reverts commit c16ae9f2010f8db9ca28169c7a98e50a8bae3335, reversing
    changes made to d97c45c2ce69b506805f50bce97e3cabd57b606d.

--- Assets/Script/MasterSceneChange.cs
+++ Assets/Script/MasterSceneChange.cs
@@ -9,76 +9,29 @@ public class MasterSceneChange : MonoBehaviour {
 
 	//public Timer t;
 
-	public AudioClip ClickSound;
-	public AudioClip BackButtonSound;
-	public AudioClip BlackMarketSound;
-	public AudioClip BuySound;
-	public AudioClip BGMusic;
-
-	/*public static MasterSceneChange MSC;
-
-	private void Awake()
-	{
-		if(MSC == null)
-		{
-			MSC = this;
-			DontDestroyOnLoad(gameObject);
-		}else
-		{
-			Destroy(gameObject);
-		}
-	}*/
-
-	void Start() {
-		if (SceneManager.GetActiveScene ().name == "BlackMarket") {
-			StartCoroutine (WaitBlackMarketSound ());
-		} else {
-			BackMusic.instance.PlayMusic (BGMusic);
-		}
-
-
-	}
-
 	public void ToGameplay(){
-		SFX.instance.source.PlayOneShot (BackButtonSound);
 		SceneManager.LoadScene("S1");
 	}
 	public void ToCrafting(){
-		//SFX.instance.source.PlayOneShot (ClickSound);
 		SceneManager.LoadScene("Crafting");
 	}
 	public void ToCrafting2Complete(){
-		//SFX.instance.source.PlayOneShot (ClickSound);
 		SceneManager.LoadScene("Crafting2Complete");
 	}
 	public void ToInv(){
-		//SFX.instance.source.PlayOneShot (ClickSound);
 		SceneManager.LoadScene("InventoryTRUE");
 	}
-
-	public void ExitGame() {
-		//MASUKKIN PLAYER PREF SIMPEN DATABASE DISINI
-		//SFX.instance.source.PlayOneShot (ClickSound);
-		Application.Quit();
-	}
-
-	public void ToMainMenu() {
-		//SFX.instance.source.PlayOneShot (ClickSound);
-		SceneManager.LoadScene ("MainMenu");
-	}
+	
 
 	public void ToBlackMarket() {
-		//SFX.instance.source.PlayOneShot (BackButtonSound);
 		SceneManager.LoadScene ("BlackMarket");
 	}
 
 	public void ToShop() {
-		//SFX.instance.source.PlayOneShot (ClickSound);
 		SceneManager.LoadScene ("Shop");
 	}
 
 	public void ToSellingConfirmation(){
-		//SFX.instance.source.PlayOneShot (ClickSound);
 		SceneManager.LoadScene("Selling");
 	}
 
@@ -89,7 +42,6 @@ public class MasterSceneChange : MonoBehaviour {
 		//PlayerPrefs.SetFloat("Time",g);
 	}
 	public void ToInvFromS1(){
-		//SFX.instance.source.PlayOneShot (ClickSound);
 		SceneManager.LoadScene("Inv");
 		//float g = t.TrueTime;
 		//PlayerPrefs.SetFloat("Time",g);
@@ -97,29 +49,14 @@ public class MasterSceneChange : MonoBehaviour {
 	
 
 	public void ToBlackMarketFromS1() {
-		//SFX.instance.source.PlayOneShot (ClickSound);
 		SceneManager.LoadScene ("BlackMarket");
 		//float g = t.TrueTime;
 		//PlayerPrefs.SetFloat("Time",g);
 	}
 
 	public void ToShopFromS1() {
-		//SFX.instance.source.PlayOneShot (ClickSound);
 		SceneManager.LoadScene ("Shop");
 		//float g = t.TrueTime;
 		//PlayerPrefs.SetFloat("Time",g);
-	}
-
-	void Update() {
-		if (Input.GetMouseButtonDown (0)) {
-			SFX.instance.source.PlayOneShot (ClickSound);
-		}
-	}
-
-	IEnumerator WaitBlackMarketSound() {
-		BackMusic.instance.source.Stop ();
-		SFX.instance.source.PlayOneShot (BlackMarketSound);
-		yield return new WaitForSeconds (5f);
-		BackMusic.instance.PlayMusic (BGMusic);
 	}
 }
